aa3056fa43b2ed5086a39ebb7d34e024af8876fa
Atri Hegde <iamatrihegde@outlook.com>
2023-02-11 17:30:45 +0000

    (feat) intro screen

---
+++ Scenes/Intro.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("FadeIn")
+	yield(get_tree().create_timer(6), "timeout")
+	$AnimationPlayer.play("FadeOut")
+	yield(get_tree().create_timer(3), "timeout")
+	get_tree().change_scene("res://Scenes/MainMenu.tscn")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
