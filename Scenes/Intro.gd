6389559d20209337c216cfc1c843eda1ec81a957
Atri Hegde <iamatrihegde@outlook.com>
2023-02-11 19:15:54 +0000

    changed project resolution, fullscreen + borderless

--- Scenes/Intro.gd
+++ Scenes/Intro.gd
@@ -8,7 +8,6 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	OS.window_fullscreen = true
 	$AnimationPlayer.play("FadeIn")
 	yield(get_tree().create_timer(3), "timeout")
 	$AnimationPlayer.play("FadeOut")
--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -1,11 +1,6 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MarginContainer/VBoxContainer/VBoxContainer/MarginContainer/PlayButton.connect("pressed", self, "_play_button_pressed")	
@@ -22,9 +17,19 @@ func _play_button_pressed():
 	
 func _credit_button_pressed():
 	get_tree().change_scene("res://Scenes/Credits.tscn")
+	
+func _quit_button_pressed():
+	get_tree().quit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_released("ui_accept"):
 		get_tree().change_scene("res://Scenes/Game.tscn")
+		
+	if Input.is_action_pressed("ui_end"):
+		get_tree().quit()
+		
+	if Input.is_key_pressed(KEY_Q):
+		get_tree().quit()
+		
 	pass
