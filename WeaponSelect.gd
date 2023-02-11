09c0bf5074eba07fca044b46da5adfe1f4b0a3f0
annietse <annietse03@gmail.com>
2023-02-11 18:50:07 +0000

    Added arrow buttons for weapon select

--- WeaponSelect.gd
+++ WeaponSelect.gd
@@ -4,19 +4,19 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-# [name, default slot, unblocked, price, sprite]
+# [name, default slot, unlocked, price, damage, rate of fire]
 var weapons= {
-	weapon_gun =['gun', 1, true, 100, "gun"],
-	weapon_knife=['knife', 2, true, 200, "knife"],
-	weapon_hammer =['hammer', 3, true, 100, "hammer"],
-	weapon_axe=['axe', 4, true, 200, "axe"]
+	weapon_gun =['gun', 1, true, 100, 3],
+	weapon_knife=['knife', 2, true, 200, 2],
+	weapon_hammer =['hammer', 3, true, 100, 1],
+	weapon_axe=['axe', 4, true, 200]
 }
 var current_weapon = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
-	get_node(weapons.values()[current_weapon][4]).visible = true;
+	get_node(weapons.values()[current_weapon][0]).visible = true;
 #	get_node(weapons[current_weapon][4]).visible = true;
 	
 	
@@ -30,25 +30,37 @@ func array_loop():
 		current_weapon = 0
 	elif current_weapon == -1:
 		current_weapon = weapons.size()-1
-	
 
 func next_unlocked(i):
-	get_node(weapons.values()[current_weapon][4]).visible = false;
+	get_node(weapons.values()[current_weapon][0]).visible = false;
 	current_weapon = current_weapon + i
 	array_loop()
 	while weapons.values()[current_weapon][2] == false:
 		current_weapon = current_weapon + i
 		array_loop()
-	get_node(weapons.values()[current_weapon][4]).visible = true;
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	get_node(weapons.values()[current_weapon][0]).visible = true;
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_just_pressed("ui_right") :
 		next_unlocked(1)
 		print(current_weapon)
 	if Input.is_action_just_pressed("ui_left"):
-		get_node(weapons.values()[current_weapon][4]).visible = false;
 		next_unlocked(-1)
 		print(current_weapon)
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene("res://Scenes/Game.tscn")
+		pass
+		# add the select
+		# equip
+		# go to next stage
 	
 #	pass
+
+
+func _on_rightbutton_pressed():
+	next_unlocked(1)
+
+
+func _on_leftbutton_pressed():
+	next_unlocked(-1)
